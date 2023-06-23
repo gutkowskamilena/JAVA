d2c6d940ba1b8a207f0ea4aeeaf254833a785971
gutkowskamilena <gutkowskamilena@gmail.com>
2023-06-23 11:15:49 +0200

    Ekstraktor słów kluczowych TF-IDF

---
+++ src/Document.java
@@ -0,0 +1,11 @@
+public class Document {
+    String documentContent;
+
+    public String getDocumentContent() {
+        return documentContent;
+    }
+
+    public void setDocumentContent(String documentContent) {
+        this.documentContent = documentContent;
+    }
+}
